bc68b966216b787dad0b6fee6adf6aab1c2d7e11
iamByteDev <220882919+iamByteDev@users.noreply.github.com>
2026-05-04 22:45:38 +0800

    init

---
+++ src/main/main.ts
@@ -0,0 +1,18 @@
+import { moveMouse, getMousePos } from 'robotjs'
+function keepMouseAtCenter() {
+  // Set the interval so the app remains responsive
+  setInterval(() => {
+    const mouse = getMousePos() // Returns {x, y}
+
+    // Only move if it's away from the target
+    if (mouse.x !== 500 || mouse.y !== 500) {
+      moveMouse(500, 500)
+    }
+  }, 60) // 100ms is frequent enough to feel "sticky" but light on CPU
+}
+
+function castTeacher{
+    
+}
+
+keepMouseAtCenter;
