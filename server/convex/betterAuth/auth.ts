0b529b3214540878a46478f4f71006915e6fc639
Evan <evan@iamevan.dev>
2026-05-06 23:23:08 +0100

    feat: basic schema

--- server/convex/betterAuth/auth.ts
+++ server/convex/betterAuth/auth.ts
@@ -1,6 +1,6 @@
 import { createAuth } from "../auth"
 import { type GenericCtx } from "@convex-dev/better-auth"
-import { type AnyDataModel } from "convex/server"
+import { DataModel } from "../_generated/dataModel"
 
 // Export a static instance for Better Auth schema generation
-export const auth = createAuth({} as GenericCtx<AnyDataModel>)
+export const auth = createAuth({} as GenericCtx<DataModel>)
