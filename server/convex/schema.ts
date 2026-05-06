2653e80e590f62b9be386bb467b6d3a2e225af55
Evan <evan@iamevan.dev>
2026-05-06 23:58:36 +0100

    feat: clients table

--- server/convex/schema.ts
+++ server/convex/schema.ts
@@ -6,4 +6,13 @@ export default defineSchema({
     name: v.string(),
     organizationId: v.string(),
   }),
+  classClients: defineTable({
+    name: v.string(),
+    secret: v.string(),
+    classId: v.id("classes"),
+    loggedInUsername: v.optional(v.string()),
+    isOnline: v.boolean(),
+  })
+    .index("classId_name", ["classId", "name"])
+    .index("secret", ["secret"]),
 })
